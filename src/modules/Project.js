303f2ca8658f72d76e7c120daadd0849e31c7088
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-09-27 10:52:41 +0200

    Create project module

---
+++ src/modules/Project.js
@@ -0,0 +1,34 @@
+import { compareAsc, format } from 'date-fns'
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+
+export default class Project {
+    constructor(name) {
+        this.name = name;
+        this.task = [];
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(newName) {
+        this.name = newName;
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    setTasks(newTasks) {
+        this.tasks = newTasks;
+    }
+
+    addTask(newTask) {
+        this.tasks.push(newTask);
+    }
+
+    deleteTask(task) {
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+    }
+}
