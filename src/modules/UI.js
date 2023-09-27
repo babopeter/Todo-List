77ac65f823bb1882d3c5f141bf00f4df7659123a
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-09-27 10:07:36 +0200

    Create basic grid layout and fill in with dummy data

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,10 +1,28 @@
 export default class UI {
     
     static loadHomepage() {
+        const page = document.createElement('div');
+        page.classList.add('page');
+
         const title = document.createElement('div');
         title.classList.add('title');
-        title.innerHTML = _.join(['Todo', 'List'], ' ');
+        title.innerHTML = "Todo List";
 
-        document.body.appendChild(title);
+        page.appendChild(title);
+
+
+        const itemContainer = document.createElement('div');
+        itemContainer.classList.add('item-container');
+        const testItems = [1, 2, 3, 4, 5];
+
+        testItems.forEach((item) => {
+            const testItem = document.createElement('div');
+            testItem.classList.add('item');
+            testItem.innerHTML = "Todo Item";
+            itemContainer.appendChild(testItem);
+        });
+
+        page.appendChild(itemContainer);
+        document.body.appendChild(page);
     }
 }
